f0b443d423d012a53f4dc88247a2a3761a5e1862
Murali Krishna <vmurali1444@gmail.com>
2017-07-01 14:23:55 +0530

    [Murali] - adds heading, refactor

--- src/eventschedule/EventSchedule.js
+++ src/eventschedule/EventSchedule.js
@@ -10,37 +10,36 @@ import scheduleIcon from "./noun_1000573_cc.svg";
 import cup from "./cup-of-hot-chocolate.svg";
 import './EventSchedule.css';
 
+const day1 = [
+    {"image": clipboard, "time": "09.30AM", "topic": "Registrations" , "name": ""},
+    {"image": abhilash, "time": "10.00AM", "topic": "Keynote" , "name": "Ramesh Baskarasubramanian"},
+    {"image": nalini, "time": "10.15AM", "topic": "Dockerize the Tests" , "name": "Nalinikanth Meesala"},
+    {"image": cup, "time": "10.55AM", "topic": "Coffee Break" , "name": ""},
+    {"image": shiv, "time": "11.10AM", "topic": "Pyramidarize your Tests" , "name": "Shivaling Sannalli"},
+    {"image": spoonandfork, "time": "01.00PM", "topic": "Lunch Break" , "name": ""},
+    {"image": ram, "time": "02.00PM", "topic": "Test Framework Designs" , "name": "Ramprasad Yadav"},
+    {"image": ram, "time": "04.00PM", "topic": "Test Framework Designs" , "name": "Ramprasad Yadav"}
+];
 
-export default class EventSchedule extends Component {
+const day2 = [
+    {"image": clipboard, "time": "09.30AM", "topic": "Registrations" , "name": ""},
+    {"image": abhilash, "time": "10.00AM", "topic": "Keynote" , "name": "Ramesh Baskarasubramanian"},
+    {"image": nalini, "time": "10.15AM", "topic": "Dockerize the Tests" , "name": "Nalinikanth Meesala"},
+    {"image": cup, "time": "10.55AM", "topic": "Coffee Break" , "name": ""},
+    {"image": shiv, "time": "11.10AM", "topic": "Pyramidarize your Tests" , "name": "Shivaling Sannalli"},
+    {"image": spoonandfork, "time": "01.00PM", "topic": "Lunch Break" , "name": ""},
+    {"image": ram, "time": "02.00PM", "topic": "Test Framework Designs" , "name": "Ramprasad Yadav"},
+    {"image": ram, "time": "04.00PM", "topic": "Test Framework Designs" , "name": "Ramprasad Yadav"}
+];
 
-    render() {
-        const day1 = [
-            {"image": clipboard, "time": "09.30AM", "topic": "Registrations" , "name": ""},
-            {"image": abhilash, "time": "10.00AM", "topic": "Keynote" , "name": "Ramesh Baskarasubramanian"},
-            {"image": nalini, "time": "10.15AM", "topic": "Dockerize the Tests" , "name": "Nalinikanth Meesala"},
-            {"image": cup, "time": "10.55AM", "topic": "Coffee Break" , "name": ""},
-            {"image": shiv, "time": "11.10AM", "topic": "Pyramidarize your Tests" , "name": "Shivaling Sannalli"},
-            {"image": spoonandfork, "time": "01.00PM", "topic": "Lunch Break" , "name": ""},
-            {"image": ram, "time": "02.00PM", "topic": "Test Framework Designs" , "name": "Ramprasad Yadav"},
-            {"image": ram, "time": "04.00PM", "topic": "Test Framework Designs" , "name": "Ramprasad Yadav"}
-        ];
+const EventSchedule = () => (
+    <section className="event-schedule container" id="schedule">
+        <h1 className="section-heading">Event Schedule</h1>
+        <div className="event-schedule__details">
+            <Day scheduleIcon={scheduleIcon} day={1} date={"Aug 5, 2017"} schedule={day1}/>
+            <Day scheduleIcon={scheduleIcon} day={2} date={"Aug 6, 2017"} schedule={day2}/>
+        </div>
+    </section>
+);
 
-        const day2 = [
-            {"image": clipboard, "time": "09.30AM", "topic": "Registrations" , "name": ""},
-            {"image": abhilash, "time": "10.00AM", "topic": "Keynote" , "name": "Ramesh Baskarasubramanian"},
-            {"image": nalini, "time": "10.15AM", "topic": "Dockerize the Tests" , "name": "Nalinikanth Meesala"},
-            {"image": cup, "time": "10.55AM", "topic": "Coffee Break" , "name": ""},
-            {"image": shiv, "time": "11.10AM", "topic": "Pyramidarize your Tests" , "name": "Shivaling Sannalli"},
-            {"image": spoonandfork, "time": "01.00PM", "topic": "Lunch Break" , "name": ""},
-            {"image": ram, "time": "02.00PM", "topic": "Test Framework Designs" , "name": "Ramprasad Yadav"},
-            {"image": ram, "time": "04.00PM", "topic": "Test Framework Designs" , "name": "Ramprasad Yadav"}
-        ];
-
-        return (
-            <section className="event-schedule container" id="schedule">
-                <Day scheduleIcon={scheduleIcon} day={1} date={"Aug 5, 2017"} schedule={day1}/>
-                <Day scheduleIcon={scheduleIcon} day={2} date={"Aug 6, 2017"} schedule={day2}/>
-            </section>
-        );
-    }
-}
+export default EventSchedule;
