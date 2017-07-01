e6659930b810b936b391d751bc6243ebc7dd05e1
Murali Krishna <vmurali1444@gmail.com>
2017-07-01 17:59:15 +0530

    [Murali] - made FAQ full bar clickable

--- src/faq/FAQ.js
+++ src/faq/FAQ.js
@@ -5,28 +5,23 @@ import './faq.css';
 export default class Question extends Component {
     constructor() {
         super();
-        this.displayDes = this.displayDes.bind(this);
-        this.closeDes = this.closeDes.bind(this);
+        this.toggleFaqDesc = this.toggleFaqDesc.bind(this);
         this.state = { "showDesc": false };
     }
 
-    displayDes() {
-        this.setState({ "showDesc": true });
-    }
-
-    closeDes() {
-        this.setState({ "showDesc": false });
+    toggleFaqDesc() {
+        this.setState(state => ({ "showDesc": !state.showDesc }));
     }
 
     render() {
         return(
             <div className="faq">
-                <div className={this.state.showDesc ? "question active" : "question"}>
+                <div className={this.state.showDesc ? "question active" : "question"} onClick={this.toggleFaqDesc}>
                     {this.props.faq.question}
                     {
                         this.state.showDesc ?
-                            <button className="close" onClick={this.closeDes}> - </button>
-                            : <button className="open" onClick={this.displayDes}> + </button>
+                            <button className="close"> - </button>
+                            : <button className="open"> + </button>
                     }
                 </div>
                 <div className= {this.state.showDesc ? "description": "hide description"}>{this.props.faq.desc}</div>
